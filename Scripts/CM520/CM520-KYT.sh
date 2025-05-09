3136e159e3aa84733b39ce24667e8c6b912be646
wwz09 <wwz5124@gmail.com>
2025-05-09 18:04:34 +0800

    sh

--- Scripts/CM520/CM520-KYT.sh
+++ Scripts/CM520/CM520-KYT.sh
@@ -26,6 +26,7 @@ rm -rf ./feeds/extraipk/theme/luci-theme-edge
 rm -rf ./feeds/extraipk/theme/luci-theme-ifit
 rm -rf ./feeds/extraipk/theme/luci-theme-opentopd
 rm -rf ./feeds/extraipk/theme/luci-theme-neobird
+rm -rf ./feeds/luci/applications/luci-app-upnp
 
 rm -rf ./package/feeds/extraipk/luci-theme-argon-18.06
 rm -rf ./package/feeds/extraipk/luci-app-argon-config-18.06
